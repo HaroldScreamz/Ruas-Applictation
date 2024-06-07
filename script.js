08d4b701a2e72119dac209de521cc249e29d0991
HaroldScreamz <tomas.schram@outlook.com>
2024-06-07 01:33:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,9 @@ function drawCard() {
 
 function displayHands() {
     if (splitHandActive) {
-        document.getElementById('player-hand').innerHTML = `<div>${handToHTML(playerHand)}</div> <div class="spacer_split"></div> <div>${handToHTML(playerHand2)}</div>`;
+        document.getElementById('player-hand').innerHTML = `<div>${handToHTML(playerHand)}</div> 
+        <div class="spacer_split"></div> 
+        <div>${handToHTML(playerHand2)}</div>`;
     } else {
         document.getElementById('player-hand').innerHTML = handToHTML(playerHand);
     }
